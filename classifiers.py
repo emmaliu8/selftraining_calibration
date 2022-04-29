ffe1af4ae035d5348ad85587dda77b89140c6e4f
Emma Liu <emmaliu@mit.edu>
2022-04-29 02:03:20 -0400

    Initialize model params

--- classifiers.py
+++ classifiers.py
@@ -4,6 +4,9 @@ class TextClassificationModel(nn.Module):
 
     def __init__(self, embed_dim, num_class, initrange=0.5, num_layers = 1):
         super(TextClassificationModel, self).__init__()
+        self.embed_dim = embed_dim 
+        self.num_class = num_class 
+        self.initrange = initrange
         self.fc = nn.Linear(embed_dim, num_class)
         self.init_weights(initrange)
 
